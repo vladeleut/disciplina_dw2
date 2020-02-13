c454ab7dd8f129fd68edd448e4f2c64242880289
Vlademir Eleuterio <vla.eleut@gmail.com>
2020-02-12 22:19:25 -0300

    Aula 2 - Até método Post

--- Hello/src/HelloServlet.java
+++ Hello/src/HelloServlet.java
@@ -3,11 +3,16 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 
 public class HelloServlet extends HttpServlet{
-	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException{
+	//public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException{
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException{    
+        String user;
+        String surname;
+        user = request.getParameter("usuario");
+        surname = request.getParameter("sobrenome");
         //método de serviço
         PrintWriter out;
         out = response.getWriter();
-        out.println("<h1>Seja bem-vindo à API de Servlets</h1>");
+        out.println("<h1>Seja bem-vindo a API de Servlets, " + user + " " + surname + "!</h1>");
         out.close();
     }
 }
